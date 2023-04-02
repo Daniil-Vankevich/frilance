e0c31bae1b2a0eef73c305b402e5a8d4458a55b3
Daniil-Vankevich <danya.vankevich@mail.ru>
2023-04-02 20:01:22 +0300

    bemverstka2 successful done

---
+++ web/2023/bem_verstka2/bem_verstka2/js/index.js
@@ -0,0 +1,6 @@
+const headerNavigation = document.querySelector('.header__nav');
+const menuBtn = document.querySelector('.menu__btn');
+
+menuBtn.addEventListener('click', () => {
+	headerNavigation.classList.toggle('header__nav-active')
+} );
