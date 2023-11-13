ee57d803de72935c6eeeddc159331fcc402b2f1b
Daniil-Vankevich <danya.vankevich@mail.ru>
2023-11-13 21:54:06 +0300

    write code for add image in div block

--- web/2023/form_valid/form_valid/js/index.js
+++ web/2023/form_valid/form_valid/js/index.js
@@ -9,6 +9,13 @@ document.addEventListener('DOMContentLoaded', function() {
 		e.preventDefault();
 	
 		let error = formValidate(form);
+
+		if(error === 0) {
+
+		} else{
+			alert('Заполните обязательные поля формы')
+		}
+
 	}
 
 	function formValidate(form) {
@@ -35,6 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
 					}	
 			}
 		}
+		return error;
 	}
 
 	function formAddError(input) {
@@ -53,5 +61,41 @@ document.addEventListener('DOMContentLoaded', function() {
 		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
 	} 
 
+	// Получаем инпут file в переменную
+
+	const formImage = document.getElementById('formImage');
+	//Получаем див для превью в переменную
+	const formPreview = document.getElementById('formPreview');
+
+	// Слушаем изменения в инпуте file:
+
+	formImage.addEventListener('change', () => {
+		uploadFile(formImage.files[0]);
+	});
+
+	function uploadFile(file) {
+		// проверяем тип файла
+		if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+			alert('Разрешены для добавления только изображения');
+			formImage.value = '';
+			return;			
+		}
+		// проверяем размер файла (<2 MB)
+		if (file.size > 2 * 1024 * 1024) {
+			alert('Файл должен быть меньше 2 MB');
+			return;
+		}
+
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			formPreview.innerHTML = `<img src="${e.target.result}" alt = "фото">`;
+		};
+
+		reader.onerror = function(e) {
+			alert('Ошибка');
+		};
+		reader.readAsDataURL(file);
+	
+	}
 
 });
