6afa10462dbb5315bb84a1a3b0f4158ead89c266
Daniil-Vankevich <danya.vankevich@mail.ru>
2023-11-14 14:20:28 +0300

    add gif loader

--- web/2023/form_valid/form_valid/js/index.js
+++ web/2023/form_valid/form_valid/js/index.js
@@ -10,7 +10,24 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 		let error = formValidate(form);
 
+		let formData = new FormData(form);
+		formData.append('image', formImage.files[0]);
+
 		if(error === 0) {
+			form.classList.add('_sending');
+
+			// let response = await fetch('sendmail.php',{
+			// 	method: 'POST',
+			// 	body: formData
+			// });
+			// if(response.ok) {
+			// 	let result = await response.json();
+			// 	alert(result.message);
+			// 	formPreview.innerHTML = '';
+			// 	form.reset();
+			// } else {
+			// 	alert('Ошибка');
+			// }
 
 		} else{
 			alert('Заполните обязательные поля формы')
